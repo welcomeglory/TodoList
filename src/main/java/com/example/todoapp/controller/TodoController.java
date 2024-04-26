59bf58acc3d1c8afcc91fc1d8513e4d63c5f1dc4
welcomeglory <godbu2021@naver.com>
2024-04-26 22:38:47 +0900

    Initial commit

---
+++ src/main/java/com/example/todoapp/controller/TodoController.java
@@ -0,0 +1,36 @@
+package com.example.todoapp.controller;
+
+import com.example.todoapp.domain.ToDo;
+import com.example.todoapp.repository.ToDoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+// Spring MVC
+@Controller
+@RequiredArgsConstructor
+public class TodoController {
+
+    private final ToDoRepository toDoRepository;
+
+    @GetMapping("/")
+    public String index(Model model){
+        List<ToDo> todos = toDoRepository.findAll();
+        model.addAttribute("todos",todos);
+        return "todos";
+    }
+    @PostMapping("/addTodo")
+    public String addTodo(@RequestParam("todo")String todo){
+       //db에 저장할 예정.
+        ToDo toDo = new ToDo();
+        toDo.setTodo(todo);
+        toDoRepository.save(toDo);
+        return "redirect:/";
+
+    }
+}
---
+++ src/main/java/com/example/todoapp/domain/ToDo.java
@@ -0,0 +1,26 @@
+package com.example.todoapp.domain;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "Todo")
+@Table(name = "todo")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String todo;
+
+}
